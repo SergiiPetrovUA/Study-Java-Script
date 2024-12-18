91cb9c914ba3e848562c3d7bc2364bb6ce2a0dc4
Sergii Petrov <chornaradaua@gmail.com>
2024-12-18 12:46:22 +0200

    Move the ball around the field

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-console.log("environment test")
-console.log('Final environment test')
+
+
+let ball = document.getElementById('ball');
+let file = document.getElementById('field');
+file.addEventListener('mouseover' , () => {file.style.cursor = 'pointer'});
+
+file.addEventListener('mousemove' , cor);
+
+function cor(event) {
+    if(event.clientX > 38 && event.clientX < 196) {
+        file.addEventListener('click' , move);
+function move(event) {
+    console.log("X : " + event.clientX + ' ; Y : '+ event.clientY  )
+    ball.style = 'position:absolute;border:2px solid black';
+    ball.style.left = event.clientX +  - ball.offsetWidth / 2 + 'px' ;
+    ball.style.top = event.clientY +  - ball.offsetHeight / 2 + 'px'
+}
+    }
+} 
